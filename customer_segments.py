27081ecaa088dd540c509a5c6deb24637f350531
viligabi <viligabi@gmail.com>
2016-08-14 10:07:30 +0200

    data preprocessing with natural logarithm added

--- customer_segments.py
+++ customer_segments.py
@@ -185,10 +185,10 @@ pd.scatter_matrix(data, alpha = 0.3, figsize = (14,8), diagonal = 'kde');
 # In[ ]:
 
 # TODO: Scale the data using the natural logarithm
-log_data = None
+log_data = np.log(data)
 
 # TODO: Scale the sample data using the natural logarithm
-log_samples = None
+log_samples = np.log(samples)
 
 # Produce a scatter matrix for each pair of newly-transformed features
 pd.scatter_matrix(log_data, alpha = 0.3, figsize = (14,8), diagonal = 'kde');
